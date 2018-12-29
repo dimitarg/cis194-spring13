efac3234c5a13768701c36c9a6b8f8e0db896b6d
dimitarg <dimitar.georgiev@ovoenergy.com>
2018-12-29 21:04:21 +0200

    Streams

--- lec06/Fibonacci.hs
+++ lec06/Fibonacci.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 import Data.List
 
 -- ex 1
@@ -19,3 +20,28 @@ fibs2' = let
 fibs2 :: [Integer]
 --unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
 fibs2 = unfoldr (\(x, y) -> Just (x, (y, (x + y)))) (0, 1)
+
+-- ex 3
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList = unfoldr (\case Cons a as -> Just (a, as))
+
+instance Show a => Show (Stream a) where
+  show = (\x -> x ++ ", ...") . show . (take 10) . streamToList
+
+-- ex 4
+--  (a -> b -> b) -> b -> t a -> b
+
+streamRepeat :: a -> Stream a
+streamRepeat a = Cons a (streamRepeat a)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons a as) = Cons (f a) (streamMap f as)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f a = Cons a (streamFromSeed f (f a))
+
+-- ex 5
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
